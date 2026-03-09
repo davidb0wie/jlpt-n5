575377916dbdd41120db4740e7a7fa714da30b9f
davidb0wie <138737390+davidb0wie@users.noreply.github.com>
2026-03-08 21:07:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // Application JLPT N5
+const BASE = '/jlpt-n5';
+
 const app = {
     data: {
         kanji: null,
@@ -115,18 +117,18 @@ const app = {
     async loadData() {
         try {
             const [kanjiResponse, countersResponse, verbsResponse, verbsPlusResponse, adverbsResponse, particlesResponse, sentencesResponse, adjectivesResponse, grammarResponse, vocabularyResponse, kanaResponse, readingResponse] = await Promise.all([
-                fetch('data/kanji.json'),
-                fetch('data/counters.json'),
-                fetch('data/verbs.json'),
-                fetch('data/verbs-plus.json'),
-                fetch('data/adverbs.json'),
-                fetch('data/particles.json'),
-                fetch('data/sentences.json'),
-                fetch('data/adjectives.json'),
-                fetch('data/grammar.json'),
-                fetch('data/vocabulary.json'),
-                fetch('data/kana.json'),
-                fetch('data/reading.json')
+                fetch('${BASE}/data/kanji.json'),
+                fetch('${BASE}/data/counters.json'),
+                fetch('${BASE}/data/verbs.json'),
+                fetch('${BASE}/data/verbs-plus.json'),
+                fetch('${BASE}/data/adverbs.json'),
+                fetch('${BASE}/data/particles.json'),
+                fetch('${BASE}/data/sentences.json'),
+                fetch('${BASE}/data/adjectives.json'),
+                fetch('${BASE}/data/grammar.json'),
+                fetch('${BASE}/data/vocabulary.json'),
+                fetch('${BASE}/data/kana.json'),
+                fetch('${BASE}/data/reading.json')
             ]);
 
             this.data.kanji = await kanjiResponse.json();
